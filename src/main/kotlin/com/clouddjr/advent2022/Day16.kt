5f4db34d60b9aaceebea58db51314c731c9f57ad
Arkadiusz Chmura <arekchmura@gmail.com>
2022-12-17 17:31:33 +0100

    Add solution for day 16

---
+++ src/main/kotlin/com/clouddjr/advent2022/Day16.kt
@@ -0,0 +1,67 @@
+package com.clouddjr.advent2022
+
+class Day16(input: List<String>) {
+    private val valves = input.map(Valve::from).associateBy(Valve::name)
+    private val usefulValves = valves.filter { it.value.rate > 0 }
+
+    private val distances = computeDistances()
+
+    fun solvePart1() = traverse(minutes = 30)
+
+    fun solvePart2() = traverse(minutes = 26, myTurn = true)
+
+    private fun traverse(
+        minutes: Int,
+        current: Valve = valves.getValue("AA"),
+        remaining: Set<Valve> = usefulValves.values.toSet(),
+        cache: MutableMap<State, Int> = mutableMapOf(),
+        myTurn: Boolean = false
+    ): Int {
+        val currentScore = minutes * current.rate
+        val currentState = State(current.name, minutes, remaining)
+
+        return currentScore + cache.getOrPut(currentState) {
+            val maxCurrent = remaining
+                .filter { next -> distances[current.name]!![next.name]!! < minutes }
+                .takeIf { it.isNotEmpty() }
+                ?.maxOf { next ->
+                    val remainingMinutes = minutes - 1 - distances[current.name]!![next.name]!!
+                    traverse(remainingMinutes, next, remaining - next, cache, myTurn)
+                }
+                ?: 0
+            maxOf(maxCurrent, if (myTurn) traverse(minutes = 26, remaining = remaining) else 0)
+        }
+    }
+
+    private fun computeDistances(): Map<String, Map<String, Int>> {
+        return valves.keys.map { valve ->
+            val distances = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }.apply { put(valve, 0) }
+            val toVisit = mutableListOf(valve)
+            while (toVisit.isNotEmpty()) {
+                val current = toVisit.removeFirst()
+                valves[current]!!.next.forEach { neighbour ->
+                    val newDistance = distances[current]!! + 1
+                    if (newDistance < distances.getValue(neighbour)) {
+                        distances[neighbour] = newDistance
+                        toVisit.add(neighbour)
+                    }
+                }
+            }
+            distances
+        }.associateBy { it.keys.first() }
+    }
+
+    private data class State(val current: String, val minutes: Int, val opened: Set<Valve>)
+
+    private data class Valve(val name: String, val rate: Int, val next: List<String>) {
+        companion object {
+            fun from(line: String): Valve {
+                return Valve(
+                    name = line.substringAfter("Valve ").substringBefore(" "),
+                    rate = line.substringAfter("rate=").substringBefore(";").toInt(),
+                    next = line.substringAfter("to valve").substringAfter(" ").split(", ")
+                )
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/com/clouddjr/advent2022/Day16Test.kt
@@ -0,0 +1,59 @@
+package com.clouddjr.advent2022
+
+import com.clouddjr.advent2022.Resources.resourceAsListOfString
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 16")
+class Day16Test {
+    private val input = """
+        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        Valve BB has flow rate=13; tunnels lead to valves CC, AA
+        Valve CC has flow rate=2; tunnels lead to valves DD, BB
+        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+        Valve EE has flow rate=3; tunnels lead to valves FF, DD
+        Valve FF has flow rate=0; tunnels lead to valves EE, GG
+        Valve GG has flow rate=0; tunnels lead to valves FF, HH
+        Valve HH has flow rate=22; tunnel leads to valve GG
+        Valve II has flow rate=0; tunnels lead to valves AA, JJ
+        Valve JJ has flow rate=21; tunnel leads to valve II
+    """.trimIndent().lines()
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        fun `Matches example`() {
+            val answer = Day16(input).solvePart1()
+
+            assertEquals(1651, answer)
+        }
+
+        @Test
+        fun `Matches actual`() {
+            val answer = Day16(resourceAsListOfString("day16.txt")).solvePart1()
+
+            assertEquals(1488, answer)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `Matches example`() {
+            val answer = Day16(input).solvePart2()
+
+            assertEquals(1707, answer)
+        }
+
+        @Test
+        fun `Matches actual`() {
+            val answer = Day16(resourceAsListOfString("day16.txt")).solvePart2()
+
+            assertEquals(2111, answer)
+        }
+    }
+}
